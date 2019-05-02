ca854011fab74fcdd508ec051664970b0351836b
yefcion <yefcion@163.com>
2019-05-02 12:53:49 +0800

    constructor

--- 05_面向对象/src/com/company/ArgsTransfer.java
+++ 05_面向对象/src/com/company/ArgsTransfer.java
@@ -6,22 +6,26 @@ package com.company;
  *      实参：调用方法时，实际传入的参数值
  *  2.规则：Java中的参数传递机制：值传递机制
  *      1）形参是基本数据类型：将实参的值传递给形参的基本数据类型的变量
- *      2）形参是引用数据类型：
- *
- *
- *
- *
- *
- *
+ *      2）形参是引用数据类型：将实参的引用类型的变量的值（对应的对空间对象实体的首选地址值）传递给形参的引用类型变量
  */
 public class ArgsTransfer {
     public static void main(String[] args) {
         int i = 10;
         int j = 6;
         System.out.println("i:" + i + "\tj:" + j);
+
         ArgsTransfer a1 = new ArgsTransfer();
-        a1.swap(i, j);                              // 将i的值传递给m，将j的值传递给n
-        System.out.println("i:" + i + "\tj:" + j);  //结果不变
+        a1.swap(i, j);                              // 此处形参为引用数据类型
+        System.out.println("i:" + i + "\tj:" + j);  // 结果不变
+
+        ArgsTransfer tt = new ArgsTransfer();
+        DataSwap dt = new DataSwap();
+        tt.dswap(dt);
+        System.out.println("i:" + dt.i + "\tj:" + dt.j);  // 结果不变
+
+        // 值传递练习
+        Value t = new Value();
+        t.first();
     }
 
     // 定义一个交换方法
@@ -31,4 +35,36 @@ public class ArgsTransfer {
         n = temp;
         System.out.println("hi");
     }
+
+    public void dswap(DataSwap ds) {
+        int temp = ds.i;
+        ds.i = ds.j;
+        ds.j = temp;
+    }
 }
+
+class DataSwap {
+    int i = 10;
+    int j = 15;
+}
+
+class Value {
+    int i = 15;                                 // 成员变量
+
+    public void first() {
+        int i = 5;                              // 成员变量
+        Value v = new Value();                  // 堆中i值为15 【内存地址①】
+        v.i = 25;                               // ①内存地址上的i值改为25
+        second(v, i);                           // 方法中的执行结束内存地址释放
+        System.out.println(v.i);                //
+    }
+
+    public void second(Value v, int i) {
+        i = 0;                                  // 成员变量
+        v.i = 20;                               // ①内存地址上的i值改为20
+        Value val = new Value();                // 堆中i值为15 【内存地址②】
+        v = val;                                // v的①内存地址直接抹除，使用val的②内存地址
+        System.out.println(v.i + " " + i);      // 15 0
+    }
+}
+
