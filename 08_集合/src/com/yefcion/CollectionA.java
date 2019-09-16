109a22b7ae1dc93b64080e62c0e98d5801d1e63e
yefcion <yefcion@163.com>
2019-09-16 23:24:46 +0800

    集合相关

---
+++ 08_集合/src/com/yefcion/CollectionA.java
@@ -0,0 +1,14 @@
+package com.yefcion;
+
+/*
+ * 1.存储对象可以考虑：1 数组  2 集合
+ * 2.数组存储对象的特点：Student[] stu = new Student[20]; stu[0] = new Student();
+ *   > 弊端：1 一旦创建，长度不可变；2 真实的数组存放的对象个数不可知
+ * 3.集合
+ *
+ *
+ * */
+
+public class CollectionA {
+    @Test
+}
