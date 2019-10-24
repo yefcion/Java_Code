ab7f77c4f881ebf53634abb031e46dc9f44821e6
yefcion <yefcion@163.com>
2019-10-25 00:10:33 +0800

    polygon modify

--- MyTools/src/main/java/application/Polygon.java
+++ MyTools/src/main/java/application/Polygon.java
@@ -6,6 +6,9 @@ import tools.Point;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Math.max;
+import static jdk.nashorn.internal.objects.NativeMath.min;
+
 /**
  * @author： xu.yefcion
  * @description： 判断一个点是否在多边形内部 - 射线法思路 https://blog.csdn.net/qq_27161673/article/details/52973866
@@ -16,26 +19,38 @@ public class Polygon {
     public void inPolygon(Point p, List<Point> poly) {
         int nMix = 0;
         for (int i = 0; i < poly.size(); i++) {
+            int j = (i + 1) % poly.size();
             double py = p.getY();
             double px = p.getX();
             double p1y = poly.get(i).getY();
-            double p2y = poly.get(i+1).getY();
+            double p1x = poly.get(i).getX();
+            double p2y = poly.get(j).getY();
+            double p2x = poly.get(j).getX();
 
-            if (p1y == p2y){
+            if (p1y == p2y) {
                 continue;
             }
-            if (py<p1y && py<p2y){
+            if (py < min(p1y, p2y)) {
                 continue;
             }
-            if (py>p1y && py>p2y){
+            if (py > max(p1y, p2y)) {
                 continue;
             }
+            Double x = (p1y - py) / (p1y - p2y) * (p2x - p1x) + p1x;
+            if (x > px) {
+                nMix++;
+            }
+        }
+        if (nMix % 2 == 1) {
+            System.out.println("the point " + p + " is in the polygon");
+        } else {
+            System.out.println("the point " + p + " is out of the polygon");
         }
     }
 
     @Test
-    public void inPolygonTest(){
-        Point p = new Point(10.3, 23.7);
+    public void inPolygonTest() {
+        Point p = new Point(70.3, 43.7);
         Point p1 = new Point(0, 0);
         Point p2 = new Point(0, 50);
         Point p3 = new Point(50, 50);
@@ -48,5 +63,7 @@ public class Polygon {
         poly.add(p3);
         poly.add(p4);
         poly.add(p5);
+
+        inPolygon(p, poly);
     }
 }
