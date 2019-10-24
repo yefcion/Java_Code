e62145196b0ea38bfa246cc44de70b342e4ddb45
yefcion <yefcion@163.com>
2019-10-25 00:19:40 +0800

    about Scanner

---
+++ MyTools/src/main/java/tools/Stage.java
@@ -0,0 +1,19 @@
+package tools;
+
+import java.util.Scanner;
+
+/**
+ * @author： xu.yefcion
+ * @description：
+ * @date： 2019/10/25 0:14
+ */
+
+public class Stage {
+    public void scannerIn(){
+        Scanner s = new Scanner(System.in);
+        //字符串
+        String str = s.next();
+        //整型
+        int score = s.nextInt();
+    }
+}
