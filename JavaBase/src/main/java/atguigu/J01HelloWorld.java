ecd9cd4f7ead63bc9dd22f228ab9616fc5bd9b2f
yefcion <yefcion@163.com>
2019-10-26 22:47:48 +0800

    整理学习文档

---
+++ JavaBase/src/main/java/atguigu/J01HelloWorld.java
@@ -0,0 +1,14 @@
+package atguigu;
+
+/**
+ * @author： xu.yefcion
+ * @description：
+ * @date： 2019/10/26 22:41
+ */
+
+public class J01HelloWorld {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hello world");
+    }
+}
