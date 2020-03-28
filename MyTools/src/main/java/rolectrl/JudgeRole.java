9c94e586febe885425b8ad686b96c78d54f65f38
yefcion <yefcion@163.com>
2020-03-28 20:42:53 +0800

    my dinner

--- MyTools/src/main/java/rolectrl/JudgeRole.java
+++ MyTools/src/main/java/rolectrl/JudgeRole.java
@@ -2,6 +2,9 @@ package rolectrl;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author： xu.yefcion
  * @description：
@@ -28,5 +31,32 @@ public class JudgeRole {
         System.out.println(s);
         System.out.println(l);
         System.out.println(k);
+    }
+
+    @Test
+    public void eat(){
+        List<String> breakfast = new ArrayList<>();
+        breakfast.add("煮粥+红枣");
+        breakfast.add("煮鸡蛋");
+        breakfast.add("清水面条");
+        breakfast.add("蒸包子");
+        breakfast.add("面包+牛奶");
+
+        List<String> lunch = new ArrayList<>();
+        lunch.add("煎鸡胸肉");
+        lunch.add("西兰花");
+        lunch.add("青椒肉丝");
+
+        List<String> dinner = new ArrayList<>();
+        dinner.add("西红柿鸡蛋面");
+        dinner.add("速冻水饺");
+
+        for (String a:breakfast){
+            for (String b:lunch){
+                for (String c:dinner){
+                    System.out.println(a+"\t"+b+"\t"+c);
+                }
+            }
+        }
     }
 }
